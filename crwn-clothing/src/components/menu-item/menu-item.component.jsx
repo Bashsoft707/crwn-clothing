7df14ba4a3c3c601a94d817e0e705f6a956be3a3
Bashsoft707 <alausabashir@gmail.com>
2021-03-01 00:49:53 -1200

    update our hats component

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.style.scss';
 
-const MenuItem = ({title, imageUrl, size }) => ( 
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match }) => ( 
     <div className={`${size} menu-item` } style={{
         backgroundImage: `url(${imageUrl})`
-    }}> 
+    }} onClick={() => history.push(`${match.url}${linkUrl}`)}> 
         <div className={`${size} background-image` } style={{
             backgroundImage: `url(${imageUrl})`
         }}></div> 
@@ -15,4 +16,4 @@ const MenuItem = ({title, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem);
